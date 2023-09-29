5f6ca7488488455192c153977dafa25e5e0dc657
Willian Assunção <willian.mican23@gmail.com>
2023-09-28 21:52:56 -0300

    adicionado regras de login

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -6,5 +6,67 @@ export const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState();
 
-    return <AuthContext.Provider>{ children }</AuthContext.Provider>;
+    useEffect(() => {
+        const userToken = localStorage.getItem("user_token");
+        const userStorage = localStorage.getItem("user_db");
+
+        if(userStorage && userStorage){
+            const hasUser = JSON.parse(userStorage)?.filter(
+                (user) => user.email === JSON.parse(userToken).email
+            );
+            
+            if(hasUser) setUser(hasUser[0]);
+        }
+    }, []);
+
+    const signin = (email, password) => {
+        const userStorage = JSON.parse(localStorage.getItem("user_db"))
+
+        const hasUser = userStorage?.filter((user) => user.email === email);
+
+        if(hasUser?.length) {
+            if(hasUser[0].email === email && hasUser[0].password === password) {
+                const token = Math.random().toString(36).substring(2);
+                localStorage.setItem("user_token", JSON.stringify({email, token}));
+
+                setUser({email, password});
+                return;
+            } else{
+                return "E-mail ou senha incorretos"
+            }
+        } else{
+            return "Usuário não encontrado";
+        }
+    };
+
+    const signup = (email, password) => {
+        const userStorage = JSON.parse(localStorage.getItem("user_db") )
+
+        const hasUser = userStorage?.filter((user) => user.email === email);
+
+        if(hasUser?.length) {
+            return "Já tem uma conta com esse E-mail";
+        }
+
+        let newUser;
+
+        if(userStorage) {
+            newUser = [...userStorage, {email, password}]
+        }else{
+            newUser = [{email, password}];
+        }
+
+        localStorage.setItem("users_db", JSON.stringify(newUser));
+
+        return
+    };
+
+    const sigout = () => {
+        setUser(null);
+        localStorage.removeItem("user_token");
+    }
+
+    return <AuthContext.Provider
+        value={{user, signed: !!user, signin, signup, sigout}}
+    >{ children }</AuthContext.Provider>;
 }
